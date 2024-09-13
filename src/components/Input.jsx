e2fc34f6a85041de2daee78e0161d62727e23abe
user <user@usernoMacBook-Air.local>
2024-09-13 16:08:05 +0900

    noProjectSelected

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,11 +5,13 @@ export default function Input({label,textarea,...props}){
     return(
         <p className="flex flex-col gap-1 my-4">
             <label className=
-            "text-sm font-bold uppercase text-stone-500"
-            >{label}</label>
-            {textarea? (<textarea {...props}  
-            className={classes}
-            />): (<input {...props } className={classes} />)}
+            "text-sm font-bold uppercase text-stone-500">
+                {label}
+            </label>
+
+            {textarea? 
+            (<textarea {...props}  className={classes}/>) : 
+            (<input {...props } className={classes} />)}
         </p>
     )
 }
