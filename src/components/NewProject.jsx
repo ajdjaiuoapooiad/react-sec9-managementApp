7e5b593ee96b03c9f3017c06793df7996712ed77
user <user@usernoMacBook-Air.local>
2024-09-14 13:18:49 +0900

    css

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -34,9 +34,9 @@ export default function NewProject({onAdd}){
         <>
         {/* error文 */}
         <Modal ref={modal} buttonCaption='Okay'>
-            <h2 className="text-xl font-bold text-stone-500 my-4">Invalid Input</h2>
-            <p className='text-stone-400  mb-4'>Oops ... looks like you forgot to enter a value.</p>
-            <p className='text-stone-400  mb-4'>Please make sure you provide a valid value for every input field.</p>
+            <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+            <p className='text-stone-600  mb-4'>Oops ... looks like you forgot to enter a value.</p>
+            <p className='text-stone-600  mb-4'>Please make sure you provide a valid value for every input field.</p>
         </Modal>
             <div className="w-[35rem] mt-16">
                 <menu className="flex items-center justify-end gap-4 my-4">
