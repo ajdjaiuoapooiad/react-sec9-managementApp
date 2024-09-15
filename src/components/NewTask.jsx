1d109bac5387c33cf8c56bcdb9055369aec8cc66
user <user@usernoMacBook-Air.local>
2024-09-15 11:03:22 +0900

    162ch finish

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export default function NewTask({onAdd}){
     //inputタグの中身
-    const [enteredTask,setEnteredTask] = useState()
+    const [enteredTask,setEnteredTask] = useState('')
 
     function handleChange(event){
         setEnteredTask(event.target.value);
